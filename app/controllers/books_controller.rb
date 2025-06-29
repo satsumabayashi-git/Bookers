eefd85419f7bdf0f6a9c243e35cd37b82b19dea3
EC2 Default User <ec2-user@ip-172-31-32-156.ap-northeast-1.compute.internal>
2025-06-29 05:21:34 +0000

    [Add] root-path,flash-message,index_<table>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  def top
+  end
+  
   def index
     @books = Book.all
     @book = Book.new
@@ -7,8 +10,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      # flash.now[:notice] = "投稿に失敗しました"
       @books = Book.all
       render :index
     end
@@ -25,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -34,6 +40,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
